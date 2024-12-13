4f00f1ab796fe0c11b184326f6068b7bc78fd844
CTZDev <carlosteranzavaleta@hotmail.com>
2024-12-13 13:16:35 -0500

    Add interface for type Person

---
+++ frontend/src/types/Person.ts
@@ -0,0 +1,11 @@
+export interface Person {
+  id: string;
+  dni: string;
+  firstName: string;
+  lastName?: string;
+  phone?: string;
+  birthDate?: string;
+  gender?: string;
+  direction?: string;
+  userId: string;
+}
